e9f89f2c59ea69668592425090aa929a9a3dbf40
ekworthi <ekworthi@mtu.edu>
2026-02-25 00:22:28 -0500

    Add automation message to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Pipeline fixed and working!");
+        System.out.println("and maybe automation as well");
     }
 }
